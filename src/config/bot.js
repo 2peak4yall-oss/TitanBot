0eb70a2f5cfc0a2d51d0848e22185fb46ec7032a
2peak4yall-oss <kekenj.alex@gmail.com>
2026-06-19 19:49:36 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Follow the rules",
+        name: "Valence RP FR",
         // Activity type number (0 = Playing).
         type: 0, 
       },
